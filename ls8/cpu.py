d491232b421fe55f6840320df2c7f48fe03c9090
GianBarGian <giacomobenati@hotmail.it>
2019-11-15 08:42:02 +0100

    day 3-4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,11 +13,18 @@ class CPU:
         self.reg = [0]*8
         self.PC = 0
         self.address = 0
+        self.SP = self.reg[7] = 0xF4
+        self.set_PC = False
         self.branchtable = {
             0b00000001: self.HLT_handle,
             0b10000010: self.LDI_handle,
             0b01000111: self.PRN_handle,
             0b10100010: self.MUL_handle,
+            0b10100000: self.ADD_handle,
+            0b01000101: self.PUSH_handle,
+            0b01000110: self.POP_handle,
+            0b01010000: self.CALL_handle,
+            0b00010001: self.RET_handle
         }
 
     def ram_read(self, address):
@@ -69,12 +76,36 @@ class CPU:
 
     def HLT_handle(self, *args):
         sys.exit(1)
+        self.set_PC = False
     def LDI_handle(self, operand_a, operand_b, *args):
         self.reg[operand_a] = operand_b
+        self.set_PC = False
     def PRN_handle(self, operand_a, *args):
         print(self.reg[operand_a])
+        self.set_PC = False
     def MUL_handle(self, operand_a, operand_b, *args):
-        self.alu('MUL', operand_a, operand_b) 
+        self.alu('MUL', operand_a, operand_b)
+        self.set_PC = False
+    def ADD_handle(self, operand_a, operand_b, *args):
+        self.alu('ADD', operand_a, operand_b)
+        self.set_PC = False
+    def PUSH_handle(self, operand_a, *args):
+        self.SP -= 1
+        self.ram[self.SP] = self.reg[operand_a]
+        self.set_PC = False
+    def POP_handle(self, operand_a, *args):
+        self.reg[operand_a] = self.ram[self.SP]
+        self.SP += 1
+        self.set_PC = False
+    def CALL_handle(self, operand_a, *args):
+        self.SP -=1
+        self.ram[self.SP] = self.PC + 2
+        self.PC = self.reg[operand_a]
+        self.set_PC = True
+    def RET_handle(self, *args):
+        self.PC = self.ram[self.SP]
+        self.SP += 1
+        self.set_PC = True
 
     def run(self):
         while True:
@@ -83,7 +114,6 @@ class CPU:
             operands_num = IR >> 6
             operand_a = self.ram_read(self.PC + 1)
             operand_b = self.ram_read(self.PC + 2)
-          
             self.branchtable[IR](operand_a, operand_b)
-           
-            self.PC += (operands_num + 1)
+            if not self.set_PC:
+                self.PC += (operands_num + 1)
