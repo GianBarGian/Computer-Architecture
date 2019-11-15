d61671a7c3d8e3f12167e60cfe1acff96b4448ef
GianBarGian <giacomobenati@hotmail.it>
2019-11-15 09:57:42 +0100

    MVP for SC done + alu operations(STRETCH)

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -15,6 +15,7 @@ class CPU:
         self.address = 0
         self.SP = self.reg[7] = 0xF4
         self.set_PC = False
+        self.FL = 0
         self.branchtable = {
             0b00000001: self.HLT_handle,
             0b10000010: self.LDI_handle,
@@ -24,7 +25,19 @@ class CPU:
             0b01000101: self.PUSH_handle,
             0b01000110: self.POP_handle,
             0b01010000: self.CALL_handle,
-            0b00010001: self.RET_handle
+            0b00010001: self.RET_handle,
+            0b10100111: self.CMP_handle,
+            0b01010100: self.JMP_handle,
+            0b01011010: self.JGE_handle,
+            0b01010110: self.JNE_handle,
+            0b01010101: self.JEQ_handle,
+            0b10101000: self.AND_handle,
+            0b10101010: self.OR_handle,
+            0b10101011: self.XOR_handle,
+            0b01101001: self.NOT_handle,
+            0b10101100: self.SHL_handle,
+            0b10101101: self.SHR_handle,
+            0b10100100: self.MOD_handle
         }
 
     def ram_read(self, address):
@@ -50,7 +63,30 @@ class CPU:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "CMP": 
+            if self.reg[reg_a] > self.reg[reg_b]:
+                self.FL = 0b00000010
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.FL = 0b00000100
+            else:
+                self.FL = 0b00000001
+        elif op == 'AND':
+            self.reg[reg_a] &= self.reg[reg_b]
+        elif op == 'OR':
+            self.reg[reg_a] |= self.reg[reg_b]
+        elif op == 'XOR':
+            self.reg[reg_a] ^= self.reg[reg_b]
+        elif op == 'NOT':
+            self.reg[reg_a] = ~self.reg[reg_a]
+        elif op == 'SHL':
+            self.reg[reg_a] << self.reg[reg_b]
+        elif op == 'SHR':
+            self.reg[reg_a] >> self.reg[reg_b]
+        elif op == 'MOD':
+            if self.reg[reg_b] == 0: 
+                print('You cannnot divide by 0')
+            else:
+                self.reg[reg_a] %= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -106,7 +142,46 @@ class CPU:
         self.PC = self.ram[self.SP]
         self.SP += 1
         self.set_PC = True
-
+    def CMP_handle(self, operand_a, operand_b, *args):
+        self.alu('CMP', operand_a, operand_b)
+        self.set_PC = False
+    def JMP_handle(self, operand_a, *args):
+        self.PC = self.reg[operand_a]
+        self.set_PC = True
+    def JGE_handle(self, operand_a, *args):
+        if self.FL == 0b00000010 or self.FL == 0b00000001:
+            self.PC = self.reg[operand_a]
+            self.set_PC = True
+    def JEQ_handle(self, operand_a, *args):
+        if self.FL == 1:
+            self.PC = self.reg[operand_a]
+            self.set_PC = True
+    def JNE_handle(self, operand_a, *args):
+        if self.FL != 0b00000001: 
+            self.PC = self.reg[operand_a]
+            self.set_PC = True
+    def AND_handle(self, operand_a, operand_b, *args):
+        self.alu('AND', operand_a, operand_b)
+        self.set_PC = False
+    def OR_handle(self, operand_a, operand_b, *args):
+        self.alu('OR', operand_a, operand_b)
+        self.set_PC = False
+    def XOR_handle(self, operand_a, operand_b, *args):
+        self.alu('XOR', operand_a, operand_b)
+        self.set_PC = False
+    def NOT_handle(self, operand_a, operand_b, *args):
+        self.alu('NOT', operand_a, operand_b)
+        self.set_PC = False
+    def SHL_handle(self, operand_a, operand_b, *args):
+        self.alu('SHL', operand_a, operand_b)
+        self.set_PC = False
+    def SHR_handle(self, operand_a, operand_b, *args):
+        self.alu('SHR', operand_a, operand_b)
+        self.set_PC = False
+    def MOD_handle(self, operand_a, operand_b, *args):
+        self.alu('MOD', operand_a, operand_b)
+        self.set_PC = False
+    
     def run(self):
         while True:
 
